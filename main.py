b0d991e17496924eb2f96b4273bdddf40fbe123d
MC1904 <miguelchern@gmail.com>
2024-04-17 00:28:33 +0800

    added letter count function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
   word_count(text)
+  print(letter_count(text))
   # textile = text.split()
   # print(len(text.split()))
 
@@ -20,5 +21,13 @@ def word_count(text):
       count += 1
   print(len(list)-count)
   return len(list)
+
+def letter_count(text):
+  letter_dict = {"a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, "l":0, "m":0, "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0}
+  text_lower = text.lower()
+  for i in range(len(text_lower)):
+    if text_lower[i] in letter_dict:
+      letter_dict[text_lower[i]] += 1
+  return letter_dict
 
 main()
